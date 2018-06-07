d47059fa2b2147663f661ce653af64f63e3715f6
Volodymyr Simkiv <volodymyr.simkiv@gmail.com>
2018-06-07 04:29:18 +0300

    fix bug with cyclic dependencies

--- mosquito-transformer/src/main/java/com/softserve/mosquito/transformer/impl/TaskTransformerImpl.java
+++ mosquito-transformer/src/main/java/com/softserve/mosquito/transformer/impl/TaskTransformerImpl.java
@@ -2,17 +2,15 @@ package com.softserve.mosquito.transformer.impl;
 
 import com.softserve.mosquito.entities.Priority;
 import com.softserve.mosquito.services.api.*;
-//import com.softserve.mosquito.services.impl.UserServiceImpl;
 import com.softserve.mosquito.transformer.api.TaskTransformer;
 import com.softserve.mosquito.dtos.TaskDto;
 import com.softserve.mosquito.entities.Task;
+import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.beans.factory.annotation.Autowired;
 
 public class TaskTransformerImpl implements TaskTransformer {
-    /*Using this code provokes cyclic dependencies between modules:
-        mosquito-transformer and mosquito-service-impl
-    private UserServiceImpl userService;
 
+    private UserService userService;
     private TaskServiceUsingEntity taskServiceUsingEntity;
     private EstimationService estimationService;
     private PriorityService priorityService;
@@ -20,7 +18,7 @@ public class TaskTransformerImpl implements TaskTransformer {
     private CommentService commentService;
 
     @Autowired
-    public TaskTransformerImpl(UserServiceImpl userService, TaskServiceUsingEntity taskServiceUsingEntity,
+    public TaskTransformerImpl(UserService userService, TaskServiceUsingEntity taskServiceUsingEntity,
                                EstimationService estimationService, PriorityService priorityService,
                                StatusService statusService, CommentService commentService) {
         this.userService = userService;
@@ -30,22 +28,21 @@ public class TaskTransformerImpl implements TaskTransformer {
         this.statusService = statusService;
         this.commentService = commentService;
     }
-    */
+
     @Override
     public Task toEntity(TaskDto taskDto) {
         Task task = new Task();
         task.setId(taskDto.getId());
         task.setName(taskDto.getName());
 
-        /* Using this code provokes cyclic dependencies between modules:
-        mosquito-transformer and mosquito-service-impl
+
         task.setParentTask(taskServiceUsingEntity.read(taskDto.getParentId()));
 
         task.setOwner(userService.getUserById(taskDto.getOwnerId()));
         task.setWorker(userService.getUserById(taskDto.getWorkerId()));
 
         task.setEstimation(estimationService.getEstimationById(taskDto.getEstimationId()));
-        */
+
 
 
         /* There is no method in PriorityService which returns object of class Priority
@@ -58,23 +55,11 @@ public class TaskTransformerImpl implements TaskTransformer {
         task.setStatus(statusService.getStatusById(taskDto.getStatusId()));
         */
 
-        /*
         task.setComments(commentService.getAllComments());
         task.setChildTasks(taskServiceUsingEntity.readAll());
-        */
 
         return task;
     }
-
-
-//            task.setEstimation(new Estimation(taskCreateDto.getEstimation()));
-//            task.setOwnerId(taskCreateDto.getOwnerId());
-//            task.setWorkerId(taskCreateDto.getWorkerId());
-//            task.setParentId(taskCreateDto.getParentId());
-//            task.setStatus(new Status(taskCreateDto.getStatusId()));
-//            task.setPriority(new Priority(taskCreateDto.getPriorityId()));
-//            return task;*/
-//        }
 
 
     @Override
