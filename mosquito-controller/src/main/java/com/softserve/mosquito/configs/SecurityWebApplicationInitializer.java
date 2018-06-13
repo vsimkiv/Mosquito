d6852264afd8c31f586c0abda9a92416c06c8d8c
Volodymyr Simkiv <volodymyr.simkiv@gmail.com>
2018-06-13 19:18:21 +0300

    add blank module mosquito-config

--- mosquito-controller/src/main/java/com/softserve/mosquito/configs/SecurityWebApplicationInitializer.java
+++ mosquito-controller/src/main/java/com/softserve/mosquito/configs/SecurityWebApplicationInitializer.java
@@ -1,7 +1,5 @@
 package com.softserve.mosquito.configs;
 
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
 public class SecurityWebApplicationInitializer
         // TODO: Uncomment. Disable security for easy testing.
         /*extends AbstractSecurityWebApplicationInitializer */{
