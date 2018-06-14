98947121b5f0651fabc1f4d2c03a9d889c4f9459
Sergijv <Sergijv@gmail.com>
2018-06-15 01:50:34 +0300

    Fb API for User by Email

--- mosquito-controller/src/main/java/com/softserve/mosquito/controllers/UserFBControler.java
+++ mosquito-controller/src/main/java/com/softserve/mosquito/controllers/UserFBControler.java
@@ -11,8 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-//@RequestMapping(value = "/social/facebook")
-// @RequestMapping(value = "/signin", method = RequestMethod.GET)
+
 
 @RestController
 @RequestMapping("/social/facebook")
