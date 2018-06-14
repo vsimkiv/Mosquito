00a5567b39449a8c2c440e95c462256611f04fc3
Makh Vitalii <patriot02faqq@gmail.com>
2018-06-14 11:04:48 +0300

    some change before going to another branch

--- mosquito-repo-impl/src/test/com/softserve/mosquito/repo/impl/UserRepoImplTest.java
+++ mosquito-repo-impl/src/test/com/softserve/mosquito/repo/impl/UserRepoImplTest.java
@@ -1,5 +1,9 @@
 package com.softserve.mosquito.repo.impl;
 
+import com.fasterxml.classmate.AnnotationConfiguration;
+import com.softserve.mosquito.entities.User;
+import org.hibernate.SessionFactory;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,15 +11,41 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import static org.junit.Assert.*;
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {TestConfiguration.class})
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class UserRepoImplTest {
 
-    @Autowired
+
+    SessionFactory sessionFactory;
+
     UserRepoImpl userRepo;
+
+    @Before
+    public void setUp() throws Exception {
+        AnnotationConfiguration configuration = new AnnotationConfiguration();
+
+        configuration.addAnnotatedClass(SuperHero.class)
+
+                .addAnnotatedClass(SuperPower.class)
+
+                .addAnnotatedClass(SuperPowerType.class);
+
+        configuration.setProperty("hibernate.dialect",
+
+                "org.hibernate.dialect.H2Dialect");
+
+        configuration.setProperty("hibernate.connection.driver_class",
+
+                "org.h2.Driver");
+
+        configuration.setProperty("hibernate.connection.url", "jdbc:h2:mem");
+
+        configuration.setProperty("hibernate.hbm2ddl.auto", "create");
+
+        sessionFactory = configuration.buildSessionFactory();
+        userRepo = new UserRepoImpl(sessionFactory);
+    }
 
     @Test
     public void create() throws Exception {
@@ -23,6 +53,7 @@ public class UserRepoImplTest {
 
     @Test
     public void read() throws Exception {
+        User users = userRepo.read(42L);
     }
 
     @Test
