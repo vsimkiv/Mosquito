814998eed5a2034bedf2d49d9fb4ee034ce7cc51
Volodymyr Barta <vol14777@gmail.com>
2018-06-16 12:24:15 +0300

    Added validation to AuthController, added UserLoginDto

--- mosquito-controller/src/main/java/com/softserve/mosquito/controllers/AuthController.java
+++ mosquito-controller/src/main/java/com/softserve/mosquito/controllers/AuthController.java
@@ -1,8 +1,10 @@
 package com.softserve.mosquito.controllers;
 
 import com.softserve.mosquito.dtos.UserDto;
+import com.softserve.mosquito.dtos.UserLoginDto;
 import com.softserve.mosquito.security.JwtAuthenticationResponse;
 import com.softserve.mosquito.security.JwtTokenProvider;
+import com.softserve.mosquito.security.LoginResponse;
 import com.softserve.mosquito.services.api.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -17,9 +19,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/")
@@ -40,7 +41,7 @@ public class AuthController {
     }
 
     @PostMapping("/signin")
-    public ResponseEntity<?> authenticateUser(@RequestBody UserDto loginRequest) {
+    public ResponseEntity<?> authenticateUser(@RequestBody @Valid UserLoginDto loginRequest) {
         Authentication authentication = null;
         try{
             authentication = authenticationManager.authenticate(
@@ -60,18 +61,23 @@ public class AuthController {
     }
 
     @PostMapping("/signup")
-    public ResponseEntity<?> registerUser(@RequestBody  UserDto signUpRequest) {
+    public ResponseEntity<?> registerUser(@RequestBody @Valid UserDto signUpRequest) {
         if(userService.getByEmail(signUpRequest.getEmail()) != null) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+            return new ResponseEntity(new LoginResponse(false, "Email Address already in use!"),
+                    HttpStatus.BAD_REQUEST);
+        }
+
+        // Check if password and confirmPassword are same
+        if(!signUpRequest.getPassword().equals(signUpRequest.getConfirmPassword())){
+            return new ResponseEntity(new LoginResponse(false, "Password and confirm password should be the same!"),
+                    HttpStatus.BAD_REQUEST);
         }
 
         // Creating user's account
         signUpRequest.setPassword(passwordEncoder.encode(signUpRequest.getPassword()));
         UserDto result = userService.save(signUpRequest);
-        URI location = ServletUriComponentsBuilder
-                .fromCurrentContextPath().path("/users/{id}")
-                .buildAndExpand(result.getId()).toUri();
 
-        return ResponseEntity.created(location).build();
+        UserDto returnedUser = UserDto.newBuilder().id(result.getId()).firstName(result.getFirstName()).lastName(result.getLastName()).build();
+        return ResponseEntity.status(HttpStatus.CREATED).body(returnedUser);
     }
 }
