56897517a3b0c8263bb94b0bcea36a020ff5863d
Makh Vitalii <patriot02faqq@gmail.com>
2018-06-18 03:35:19 +0300

    get swagger documentation, try to set swagger UI

--- mosquito-config/src/main/java/com/softserve/mosquito/configs/SwaggerConfig.java
+++ mosquito-config/src/main/java/com/softserve/mosquito/configs/SwaggerConfig.java
@@ -4,27 +4,35 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig extends WebMvcConfigurationSupport {
+public class SwaggerConfig {
     @Bean
-    public Docket api() {
+    public Docket api(){
         return new Docket(DocumentationType.SWAGGER_2)
-                .select().apis(RequestHandlerSelectors.any()).paths(PathSelectors.any()).build();
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
     }
 
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("swagger-ui.html")
-                .addResourceLocations("com.softserve.mosquito:/META-INF/resources/");
-
-        registry.addResourceHandler("/webjars/**")
-                .addResourceLocations("com.softserve.mosquito/META-INF/resources/webjars/");
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("TITLE")
+                .description("DESCRIPTION")
+                .version("VERSION")
+                .termsOfServiceUrl("http://terms-of-services.url")
+                .license("LICENSE")
+                .licenseUrl("http://url-to-license.com")
+                .build();
     }
 }
--- mosquito-config/src/main/java/com/softserve/mosquito/configs/WebAppConfig.java
+++ mosquito-config/src/main/java/com/softserve/mosquito/configs/WebAppConfig.java
@@ -5,9 +5,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.*;
 
 import javax.mail.Authenticator;
 import javax.mail.PasswordAuthentication;
@@ -46,4 +44,17 @@ public class WebAppConfig extends WebMvcConfigurerAdapter {
         registry.addInterceptor(new Interceptor());
     }
 
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/docApi/v2/api-docs", "/v2/api-docs");
+        registry.addRedirectViewController("/docApi/swagger-resources/configuration/ui", "/swagger-resources/configuration/ui");
+        registry.addRedirectViewController("/docApi/swagger-resources/configuration/security", "/swagger-resources/configuration/security");
+        registry.addRedirectViewController("/docApi/swagger-resources", "/swagger-resources");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/docApi/swagger-ui.html**").addResourceLocations("classpath:/META-INF/resources/swagger-ui.html");
+        registry.addResourceHandler("/docApi/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
 }
