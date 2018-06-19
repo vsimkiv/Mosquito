a70f5a1cd0226f1e13884bc5bfeb2edc8e683e9c
Volodymyr Simkiv <volodymyr.simkiv@gmail.com>
2018-06-19 22:48:23 +0300

    set additional fields in method getById() in class TaskServiceImpl. Setting Estimation remaining

--- mosquito-service-impl/src/main/java/com/softserve/mosquito/services/impl/TaskServiceImpl.java
+++ mosquito-service-impl/src/main/java/com/softserve/mosquito/services/impl/TaskServiceImpl.java
@@ -3,10 +3,14 @@ package com.softserve.mosquito.services.impl;
 import com.softserve.mosquito.dtos.TaskFullDto;
 import com.softserve.mosquito.dtos.TaskSimpleDto;
 import com.softserve.mosquito.dtos.UserDto;
+import com.softserve.mosquito.entities.Comment;
+import com.softserve.mosquito.entities.Estimation;
 import com.softserve.mosquito.entities.Task;
 import com.softserve.mosquito.repo.api.TaskRepo;
 import com.softserve.mosquito.services.api.TaskService;
 import com.softserve.mosquito.services.mail.MailSender;
+import com.softserve.mosquito.transformer.CommentTransformer;
+import com.softserve.mosquito.transformer.EstimationTransformer;
 import com.softserve.mosquito.transformer.TaskTransformer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -75,6 +79,12 @@ public class TaskServiceImpl implements TaskService {
             taskFullDto.setParentTaskFullDto(getParent(parent.getId()));
         }
 
+//        Estimation estimation = task.getEstimation();
+//        if (estimation != null) {
+//            taskFullDto.setEstimationDto(EstimationTransformer.toDTO(estimation));
+//        }
+
+        taskFullDto.setCommentDtoList(CommentTransformer.toDTOList(task.getComments()));
         taskFullDto.setChildTaskFullDtoList(getSubTasks(taskFullDto.getId()));
         return taskFullDto;
     }
