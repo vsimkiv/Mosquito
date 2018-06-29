1c0ccfb540a42c939ed66a15e78fcc30ce140a96
Vitalik Mah <patriot02faqq@gmail.com>
2018-06-30 02:54:56 +0300

    add lombok to mongo entity

--- mosquito-entity/src/main/java/com/softserve/mosquito/entities/mongo/TaskMongo.java
+++ mosquito-entity/src/main/java/com/softserve/mosquito/entities/mongo/TaskMongo.java
@@ -1,35 +1,18 @@
 package com.softserve.mosquito.entities.mongo;
 
-public class TaskMongo {
+import com.softserve.mosquito.entities.Task;
+import lombok.*;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TaskMongo {
     private Long taskId;
     private String taskName;
 
-    public TaskMongo() {}
-
-    public TaskMongo(Long taskId, String taskName) {
-        this.taskId = taskId;
-        this.taskName = taskName;
-    }
-
-    public TaskMongo(com.softserve.mosquito.entities.Task task) {
+    public TaskMongo(Task task) {
         this.taskId = task.getId();
         this.taskName = task.getName();
-    }
-
-    public Long getTaskId() {
-        return taskId;
-    }
-
-    public void setTaskId(Long taskId) {
-        this.taskId = taskId;
-    }
-
-    public String getTaskName() {
-        return taskName;
-    }
-
-    public void setTaskName(String taskName) {
-        this.taskName = taskName;
     }
 }
--- mosquito-entity/src/main/java/com/softserve/mosquito/entities/mongo/TasksBoard.java
+++ mosquito-entity/src/main/java/com/softserve/mosquito/entities/mongo/TasksBoard.java
@@ -1,47 +1,21 @@
 package com.softserve.mosquito.entities.mongo;
 
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@RequiredArgsConstructor
 @Document(collection = "TasksBoard")
 public class TasksBoard {
 
     @Id
     private String id;
-    private Long userId;
-    private List<TaskMongo> taskMongos = new ArrayList<>();
-
-    public TasksBoard() { }
-
-    public TasksBoard(Long userId, List<TaskMongo> taskMongos) {
-        this.userId = userId;
-        this.taskMongos = taskMongos;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-
-    public List<TaskMongo> getTaskMongos() {
-        return taskMongos;
-    }
-
-    public void setTaskMongos(List<TaskMongo> taskMongos) {
-        this.taskMongos = taskMongos;
-    }
+    @NonNull private Long userId;
+    @NonNull private List<TaskMongo> taskMongos = new ArrayList<>();
 }
