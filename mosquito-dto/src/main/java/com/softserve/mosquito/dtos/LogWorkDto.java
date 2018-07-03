727f0660cb55b9e30dfb222108d9f3f3723aaa71
Sergijv <Sergijv@gmail.com>
2018-07-04 00:09:59 +0300

    try to correct date

--- mosquito-dto/src/main/java/com/softserve/mosquito/dtos/LogWorkDto.java
+++ mosquito-dto/src/main/java/com/softserve/mosquito/dtos/LogWorkDto.java
@@ -3,6 +3,7 @@ package com.softserve.mosquito.dtos;
 import lombok.*;
 
 import java.time.LocalDateTime;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Builder
 @Setter
@@ -10,11 +11,13 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
+@JsonFormat(pattern = "yyyy-MM-dd@HH:mm:ss.SSSZ")
 public class LogWorkDto {
     private Long id;
     private String description;
     private Long userId;
     private int logged;
+
     private LocalDateTime lastUpdate;
     private Long estimationId;
 }
